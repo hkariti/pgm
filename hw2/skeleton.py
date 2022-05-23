86853cc9fe13c0aa4d085896fc4cbaa1dc380c0c
Dragonzlayer <mtelpaz@gmail.com>
2022-05-23 15:16:24 +0300

    pgm hw2 after some modification

--- hw2/skeleton.py
+++ hw2/skeleton.py
@@ -2,6 +2,7 @@ from pgmpy.models.MarkovNetwork import MarkovNetwork
 from pgmpy.inference import BeliefPropagation
 from pgmpy.factors.discrete import DiscreteFactor
 
+
 def Q1():
     G = MarkovNetwork()
     G.add_nodes_from(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
@@ -83,15 +84,14 @@ def Q1():
     # find probability that at least one yellow buys the product
     print('1.2.1) At least one yellow:')
     # complete
-    prob_y = belief_prop.query(variables=['a', 'h', 'e'])
-    print(prob_y)
+    prob_y = belief_prop.query(variables=['a', 'h', 'e'], show_progress=False)
+    # print(prob_y)
     print(1-prob_y.get_value(a=0, h=0, e=0))
 
     # find probability that at least two blues buy the product
     print('1.2.2) At least two blues:')
     # complete
-    prob_b = belief_prop.query(variables=['b', 'j', 'f'])
-    print(prob_b)
+    prob_b = belief_prop.query(variables=['b', 'j', 'f'], show_progress=False)
     print(1-(prob_b.get_value(b=0, j=0, f=0) + prob_b.get_value(b=1, j=0, f=0) + prob_b.get_value(b=0, j=1, f=0) +
              prob_b.get_value(b=0, j=0, f=1)))
 
@@ -104,7 +104,8 @@ def Q1():
     # most probable configuration given that all the yellows bought the product
     print('1.2.4) Most likely configuration given that all the yellows bought the product:')
     # complete
-    config_cond = belief_prop.map_query(list(set(G.nodes) - {'a', 'h', 'e'}), evidence={'a': 1, 'h': 1, 'e': 1})
+    config_cond = belief_prop.map_query(list(set(G.nodes) - {'a', 'h', 'e'}), evidence={'a': 1, 'h': 1, 'e': 1},
+                                        show_progress=False)
     print(config_cond)
 
 
@@ -118,7 +119,7 @@ def Q2():
     '''Define the node factors of the model'''
     ############################################################
     for node in G.nodes:
-        node_fact = DiscreteFactor([node], [2], [0.5, 0.5]) # just put positive numbers
+        node_fact = DiscreteFactor([node], [2], [0.5, 0.5])  # just put positive numbers
         G.add_factors(node_fact)
     ############################################################
     '''Define the edge factors of the model'''
@@ -130,8 +131,8 @@ def Q2():
         G.add_factors(factor)
 
     belief_prop = BeliefPropagation(G)
-    num_ind_sets = belief_prop.query(variables=list(G.nodes))
-    print((num_ind_sets.values > 0).sum())
+    num_ind_sets = belief_prop.query(variables=list(G.nodes), show_progress=False)
+    print( "num of independent sets: ", (num_ind_sets.values > 0).sum())
 
     # Maximum Independent Set
     print('2.2) Find the size of the largest independent set in G:')
@@ -142,7 +143,7 @@ def Q2():
                       ('e', 'f'), ('e', 'i'), ('e', 'j'), ('f', 'g'), ('f', 'j'), ('g', 'h'), ('g', 'i'), ('h', 'i')])
 
     for node in G.nodes:
-        node_fact = DiscreteFactor([node], [2], [1, 2]) # just put positive numbers
+        node_fact = DiscreteFactor([node], [2], [1, 2])  # just put positive numbers
         G.add_factors(node_fact)
 
     for edge in G.edges:
@@ -153,8 +154,10 @@ def Q2():
 
     belief_prop = BeliefPropagation(G)
 
-    size_mis = sum(belief_prop.map_query(list(G.nodes)).values())
+    size_mis = sum(belief_prop.map_query(list(G.nodes), show_progress=False).values())
+    config2 = belief_prop.map_query(list(G.nodes), show_progress=False)
     print(size_mis)
+    print("independent set of max size: ", config2)
 
 
 if __name__ == '__main__':
